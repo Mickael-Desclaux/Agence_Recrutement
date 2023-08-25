1bb5d3df1a3d32e4428f70a18a82e51fc1804820
Mickael <mickaeldesclaux@gmail.com>
2023-08-25 12:31:08 +0200

    Handle RecruiterProfileController

--- src/Controller/RecruiterProfileController.php
+++ src/Controller/RecruiterProfileController.php
@@ -2,17 +2,61 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RecruiterProfileType;
+use App\Repository\RecruiterProfileRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class RecruiterProfileController extends AbstractController
 {
-    #[Route('/recruiter/profile', name: 'app_recruiter_profile')]
-    public function index(): Response
+    #[Route('/recruiter/profile', name: 'recruiter_profile')]
+    public function profileView(RecruiterProfileRepository $recruiterProfileRepository, UserInterface $user): Response
     {
+        if (!$user instanceof User || !in_array('ROLE_RECRUITER', $user->getRoles())) {
+            throw $this->createAccessDeniedException('Accès interdit');
+        }
+
+        $recruiterProfile = $recruiterProfileRepository->findOneBy(['user' => $user]);
+
+        if (!$recruiterProfile) {
+            throw $this->createNotFoundException('Aucun profil trouvé pour ce recruteur');
+        }
+
         return $this->render('recruiter_profile/index.html.twig', [
-            'controller_name' => 'RecruiterProfileController',
+            'recruiterProfile' => $recruiterProfile,
+        ]);
+    }
+
+    #[Route('/recruiter/profile/edit', name: 'recruiter_profile_edit')]
+    public function editProfile(Request $request, UserInterface $user, RecruiterProfileRepository $recruiterProfileRepository, EntityManagerInterface $entityManager): Response
+    {
+        if (!$user instanceof User || !in_array('ROLE_RECRUITER', $user->getRoles())) {
+            throw $this->createAccessDeniedException('Accès interdit');
+        }
+
+        $recruiterProfile = $recruiterProfileRepository->findOneBy(['user' => $user]);
+
+        if (!$recruiterProfile) {
+            throw $this->createNotFoundException('Aucun profil trouvé pour ce recruteur');
+        }
+
+        $form = $this->createForm(RecruiterProfileType::class, $recruiterProfile);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('recruiter_profile');
+        }
+
+        return $this->render('recruiter_profile/edit.html.twig', [
+            'editForm' => $form->createView(),
         ]);
     }
 }
--- src/Form/RecruiterProfileType.php
+++ src/Form/RecruiterProfileType.php
@@ -4,24 +4,38 @@ namespace App\Form;
 
 use App\Entity\RecruiterProfile;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class RecruiterProfileType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-//            ->add('userID')
-            ->add('companyName')
-            ->add('companyAddress')
-        ;
+            ->add('companyName', TextType::class,
+            ['label' => 'Nom de la société',
+                'constraints' => [
+                    new Length(['max' => 50, 'maxMessage' => 'Le nom de la société ne doit pas dépasser 50 caractères'])
+                ]])
+            ->add('companyAddress', TextType::class,
+                ['label' => 'Adresse de la société',
+                    'constraints' => [
+                        new Length(['max' => 150, 'maxMessage' => 'L\'adresse de la société ne doit pas dépasser 150 caractères'])
+                    ]])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Envoyer']);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => RecruiterProfile::class,
+            'csrf_protection' => true,
+            'csrf_field_name' => '_token',
+            'csrf_token_id' => 'company_name',
         ]);
     }
 }
