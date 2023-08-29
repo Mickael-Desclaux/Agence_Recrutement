51d1ff0aeeb1dad36f392cdf16bc95b8798fbc05
Mickael <mickaeldesclaux@gmail.com>
2023-08-29 11:19:03 +0200

    Add validated jobOffers to Home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\JobOfferRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(JobOfferRepository $jobOfferRepository): Response
     {
+        $jobOffers = $jobOfferRepository->findBy(['publishValidation' => true]);
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'job_offers' => $jobOffers,
         ]);
     }
 }
